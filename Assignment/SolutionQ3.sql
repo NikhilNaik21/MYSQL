b696d0fafbaf4f36281a96451b611b47726b7d90
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-05-30 23:31:55 +0530

    Create SolutionQ3.sql

---
+++ Assignment/SolutionQ3.sql
@@ -0,0 +1,10 @@
+create table cardtype(card_id int primary key,
+    -> card_type varchar(20));
+    
+    
+insert into cardtype values(500,'Platinum');
+insert into cardtype values(600,'Silver');
+insert into cardtype values(700,'Diamond');
+
+
+select * from cardtype;
