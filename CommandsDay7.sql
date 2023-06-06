f90605d6d0f7c7b59de57399477dc1384b9d9ce3
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-06-06 20:28:16 +0530

    Update CommandsDay7.sql

--- CommandsDay7.sql
+++ CommandsDay7.sql
@@ -1,1 +1,9 @@
 
+mysql> use cdac;
+mysql> -- Integrity Constraint 
+mysql> -- 1.Entity Integrity 2.Referential Integrity 3.Domain Integrity 
+mysql> /* on delete
+   /*> cascase
+   /*> on delete set null
+   /*> on update cascade 
+   /*> */
